ca9776358c9a30371e30caab0982c82f18cec9e0
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-18 17:36:35 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
 class Meal
-
+  
+  attr_accessor :waiter, :customer, :total, :tip
+  
+  @@all = []
+  
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter 
+    @customer = customer
+    @total = total
+    @@all << self
+    @tip = tip
+  end
+  
+  def self.all
+    @@all
+  end
 end
